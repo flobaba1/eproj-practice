2569cfc33f33ab75f8f2d646698619e6de831df2
adeniyi234israel <israeladeniyi234@gmail.com>
2026-08-24 15:46:44 +0100

    Subtract

--- bin/dart_practice.dart
+++ bin/dart_practice.dart
@@ -2,4 +2,5 @@ import 'package:dart_practice/dart_practice.dart' as dart_practice;
 
 void main(List<String> arguments) {
   print('Hello world: ${dart_practice.calculate()}!');
+  print('Subtract: ${dart_practice.subtract()}!');
 }
