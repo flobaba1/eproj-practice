ccf027d10609bb689edc4af6b1a7ff8c8a07b0d9
Emmanuel Adinoyi Adaja <emmanueladaja@yahoo.com>
2026-08-24 15:40:37 +0100

    Added suare root functionality

--- bin/dart_practice.dart
+++ bin/dart_practice.dart
@@ -2,4 +2,5 @@ import 'package:dart_practice/dart_practice.dart' as dart_practice;
 
 void main(List<String> arguments) {
   print('Hello world: ${dart_practice.calculate()}!');
+  print('Square root of 16 is: ${dart_practice.squareRoot()}');
 }
--- lib/dart_practice.dart
+++ lib/dart_practice.dart
@@ -1,3 +1,8 @@
+import 'dart:math';
 int calculate() {
   return 6 * 7;
+}
+
+double squareRoot() {
+  return sqrt(16);
 }
